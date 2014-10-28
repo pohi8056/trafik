0773ad7f95b7fdcbc623248c059faaa4b3987601
lulbasaur <lukas.hamberg@gmail.com>
2014-10-28 18:41:28 +0100

    fixed poppes virus

--- CarPosition.java
+++ CarPosition.java
@@ -1,39 +1,8 @@
 
 
-//
-// H√•ller i en bil och k√§nner till sina "grannar". 
+// HÂller i en bil och k‰nner till sina "grannar". 
 public class CarPosition{
 	
-<<<<<<< HEAD
-	private Car currentCar = null; // null om ingen bil finns p√• positionen
-	
-	private Lane owner;
-	
-	private CarPosition forward;
-	private CarPosition turn;
-	
-	public CarPosition(Lane a_Owner)
-	{
-		owner = a_Owner;
-	}
-	
-	public boolean isEnd(CarPosition target)
-	{
-		return owner.matchEnd(target);
-	}
-	
-	public boolean moveForward()
-	{
-		return false;
-		// Flytta bilen fram till forward
-	}
-	
-	public boolean turn()
-	{
-		return false;
-		// Flytta bilen till turn
-	}
-=======
     private Car currentCar = null; // null om ingen bil finns pÂ positionen
 	
     private Lane owner;
@@ -62,7 +31,6 @@ public class CarPosition{
 	return false;
 	// Flytta bilen till turn
     }
->>>>>>> 5f99a3f2ba6354dddfb076c4e453ddb1fefb3801
 
     public void setTurn(CarPosition turn) {
 	this.turn = turn;
@@ -75,8 +43,9 @@ public class CarPosition{
     public CarPosition getForward() {
 	return forward;
     }
-
+    //Added updateForward
     public void updateForward(CarPosition newForward) {
 	this.forward = newForward;
     }	
 }
+
--- Lane.java
+++ Lane.java
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-public class Lane {    private     public static class OverflowException extends RuntimeException {        // Undantag som kastas när det inte gick att lägga         // in en ny bil på vägen    }    protected CarPosition[] theLane;    public Lane(int n) {	// Konstruerar ett Lane-objekt med plats för n fordon    // Samt länker ihop varje CarPosition med forward för den framför    }        public boolean matchEnd(CarPosition target)    {    	if(theLane[0] == target)    		return true;    	else    		return false;    }        public int getLength(){    	return theLane.length;    }        public void setParallel(Lane sideLane)    {    	int i = 0;    	    	while(i < sideLane.getLength() && i < theLane.length)    	{    		theLane[i].setTurn(sideLane.theLane[i]);    		i++;    	}    }    public void step() {	// Stega fram alla fordon (utom det på plats 0) ett steg         // (om det går). (Fordonet på plats 0 tas bort utifrån 	// mm h a metoden nedan.)    }    public Car getFirst() {	private int i = 0;	private int j = 0;	while(i < theLane.getLength()){	    if(theLane[i] != null){		theLane[i] = null;		j = i;	    }	    i++;	}    	return theLane[j];	// Returnera och tag bort bilen som står först    }    public Car firstCar() {	private int i = 0;	private int j = 0;	while(i < theLane.getLength()){	    if(theLane[i] != null){		j = i;	    }	    i++;	}	    	return theLane[j];	// Returnera bilen som står först utan att ta bort den    }    public boolean lastFree() {	if(theLane[])    	return false;	// Returnera true om sista platsen ledig, annars false    }    public void putLast(Car c) throws OverflowException {	// Ställ en bil på sista platsen på vägen	// (om det går).    }    public String toString() {    	return "";    	//...    	}}
-=======
+
 public class Lane {
 
     public static class OverflowException extends RuntimeException {
@@ -34,7 +32,7 @@ public class Lane {
     public void setParallel(Lane sideLane)
     {
     	int i = 0;
-    	
+        
     	while(i < sideLane.getLength() && i < theLane.length)
     	{
     		theLane[i].setTurn(sideLane.theLane[i]);
@@ -75,4 +73,3 @@ public class Lane {
     	}
 
 }
->>>>>>> 5f99a3f2ba6354dddfb076c4e453ddb1fefb3801
