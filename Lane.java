28d61ab1211a474a80115b23b4c2719e69f4df0d
poppe <philding@hotmail.com>
2014-10-28 18:55:33 +0100

    FIXED SWEDISH FUCKING STUFF

--- Lane.java
+++ Lane.java
@@ -2,15 +2,15 @@
 public class Lane {
 
     public static class OverflowException extends RuntimeException {
-        // Undantag som kastas när det inte gick att lägga 
-        // in en ny bil på vägen
+        // Undantag som kastas nar det inte gick att lagga 
+        // in en ny bil pa vagen
     }
 
     protected CarPosition[] theLane;
 
     public Lane(int n) {
-	// Konstruerar ett Lane-objekt med plats för n fordon
-    // Samt länker ihop varje CarPosition med forward för den framför
+	// Konstruerar ett Lane-objekt med plats for n fordon
+    // Samt lanker ihop varje CarPosition med forward for den framfor
 	theLane = new CarPosition[n];
 	for (int i = 0; i < n ; i++) {
 	    theLane[i].updateForward(theLane[i+1]);  
@@ -41,8 +41,8 @@ public class Lane {
     }
 
     public void step() {
-	// Stega fram alla fordon (utom det på plats 0) ett steg 
-        // (om det går). (Fordonet på plats 0 tas bort utifrån 
+	// Stega fram alla fordon (utom det pa plats 0) ett steg 
+        // (om det gar). (Fordonet pa plats 0 tas bort utifran 
 	// mm h a metoden nedan.)
     }
 
@@ -81,8 +81,8 @@ public class Lane {
     }
 
     public void putLast(Car c) throws OverflowException {
-	// Ställ en bil på sista platsen på vägen
-	// (om det går).
+	// Stall en bil pa sista platsen pa vagen
+	// (om det gar).
     }
 
     public String toString() {
